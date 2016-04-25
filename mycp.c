e995b0d9fd952631907b9ad4bef6e7338efba0cf
Sean Miller <scm84@txstate.edu>
2016-04-24 21:53:10 -0500

    mycp.c now checks for arguments properly

--- mycp.c
+++ mycp.c
@@ -3,16 +3,19 @@
 
 int main(int argc, char **argv)
 {
+	int c;
+	int Rflag = 0;
+
 	// check args
 	if (argc < 3)
 	{
-		fprintf(stderr, "error: too few arguments"\n);
-		exit(1);
+		fprintf(stderr, "error: too few arguments\n");
+		return(1);
 	}
 	if (argc > 4)
 	{
-		fprintf(stderr, "error: too many arguments"\n);
-		exit(1);
+		fprintf(stderr, "error: too many arguments\n");
+		return(1);
 	}
 
 	if(argc == 3)
@@ -20,19 +23,25 @@ int main(int argc, char **argv)
 		
 	}
 	
-	int c;
-
 	while ((c = getopt (argc, argv, "R")) != -1)
 	{
 		switch(c)
 		{
 			case 'R':
-				cpDir();
+				Rflag = 1;
 				break;
 			default:
-				cpFile();
 				break;
 		}
+	}
+
+	if (Rflag)
+	{
+		fprintf(stdout, "Copy directory %s to %s\n", argv[2], argv[3]);
+	}
+	else
+	{
+		fprintf(stdout, "Copy flag %s to %s\n", argv[1], argv[2]);
 	}
 
 	return 0;
