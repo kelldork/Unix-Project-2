e15d71ad938f4c4bd467f849b14e216ed049f613
Lais <l_d91@txstate.edu>
2016-04-27 21:42:13 -0500

    Changes for directory checking

--- mycat/mycat.c
+++ mycat/mycat.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-void processFile(FILE *);
+void processFile(FILE *, char* curFile);
 
 int main(int argc, char **argv) 
 {
+   // bypass pass the first arg(mycat) by incrementing argv pointer  
+   *argv++;
    if(argc > 1)
    {
-       // bypass pass the first arg(mycat) by incrementing argv pointer  
-       *argv++;
        // cat can have any number of files to concatenate passed to it
        while(*argv)
        {
           FILE *fp = fopen(*argv, "r");
-          // If the file doesn't exist print the message
+          // If the file open failed print the error message
           if(fp == NULL)
           {
             warn("%s", *argv);
           }
           else
           {
-             processFile(fp);
+             processFile(fp, *argv);
              fclose(fp); 
           }
-             *argv++;
+          *argv++;
         }
      }
      else
@@ -33,7 +35,7 @@ int main(int argc, char **argv)
         // then it is a file redirect or pipe 
         if(!isatty(STDIN_FILENO))
         {
-           processFile(stdin);
+           processFile(stdin, "-");
         }
         // Only 1 argument, and no stdin redirect, so args were missing
         else
@@ -43,13 +45,27 @@ int main(int argc, char **argv)
      }
 }
 
-void processFile(FILE *fp)
+void processFile(FILE *fp, char *curFile)
 {
-    int ch;
-    ch = getc(fp);
-    while(ch != EOF)
+    int fd = fileno(fp);
+    struct stat buf = {0};
+    if(-1 == fstat(fd, &buf))
     {
-       putchar(ch);
+        perror("fstat() failed");
+        exit(1);
+    }
+    else if(S_ISDIR(buf.st_mode))
+    {
+       fprintf(stderr, "mycat: %s: Is a directory\n", curFile);
+    }
+    else
+    {
+       int ch;
        ch = getc(fp);
+       while(ch != EOF)
+       {
+          putchar(ch);
+          ch = getc(fp);
+       }
     }
 }
