e0d0fe7a22a4f09ae9150e88743d0c3abe7b6923
Lais <l_d91@txstate.edu>
2016-04-27 22:54:31 -0500

    Changes to call execv and manage child processes

--- mysh/mysh.c
+++ mysh/mysh.c
@@ -1,7 +1,117 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
-int main(int argc, char **argv) 
+#define MAX_BUFF 1024
+#define MAX_ARGS 64
+#define SEPARATOR " \t\n"
+
+/**
+This only works if the executables are in the same directory
+as mysh.  In order to support paths we would have to parse
+detect myls, mycat, or mycp was being called in the shell
+even if someone types ../bin/mycat for example **/
+
+void executeProgram(char **args);
+
+int main(int argc, char ** argv) {
+  char buf[MAX_BUFF];
+  char line[MAX_BUFF];
+  char nwd[MAX_BUFF];
+  char prompt[MAX_BUFF];
+  char * args[MAX_ARGS];
+  char ** arg;
+  struct passwd *pwd;
+  struct stat uid;
+
+  while(!feof(stdin)) {
+
+    //prompt - uid:/path/$
+    getcwd(prompt, sizeof(prompt));
+    pwd = getpwuid(uid.st_uid);
+    printf("%s:%s$ ", pwd->pw_name, prompt);
+
+    if(fgets(line, MAX_BUFF, stdin)) {
+
+      arg = args;
+      *arg++ = strtok(line, SEPARATOR);
+
+      while ((*arg++ = strtok(NULL, SEPARATOR)));
+
+      if (args[0]) {
+
+        //clear screen
+        if(!strcmp(args[0], "clear")) {
+          printf("\e[1;1H\e[2J");
+        }
+
+        //mycat
+        else if(!strcmp(args[0], "mycat")) {
+          executeProgram(args);
+          //if(!args[1]) printf("mycat: %s: No such file or directory\n", args[1]);
+          //else mycat(args[1]);
+        }
+
+        //mycd
+        else if(!strcmp(args[0], "mycd")) {
+          if(!strcmp(args[1], ".")) {
+            chdir(".");
+          }
+          else if(!strcmp(args[1], "..")) {
+            chdir("..");
+          }
+          else {
+            if(chdir(args[1]) != 0) {
+              printf("-bash: mycd: %s: No such file or directory\n", args[1]);
+            }
+            else {
+              chdir(args[1]);
+            }
+          }
+        }
+
+        //pwd
+        else if(!strcmp(args[0], "pwd")) printf("%s\n", prompt);
+
+        //exit
+        else if(!strcmp(args[0], "exit")) break;
+
+        //wrong command
+        else fprintf(stderr, "%s: command not found\n", args[0]);
+      }
+    }
+  }
+  return 0;
+} 
+
+
+void executeProgram(char **args)
 {
+   pid_t pid=fork();
+   if(pid == 0)
+   {
+      int res = execv(args[0], args);
+      fprintf(stderr, "execv result %i failed with errno %d\n", res, errno);
+      //errno 2 is no such file or directory 
+      if(errno == 2)
+      {
+         fprintf(stderr, "Make sure %s is in the same directory as mysh\n", args[0]);
+      }
+      exit(127);
+   }
+   else if(pid > 0)
+   {
+      // wait for child to exit
+      waitpid(pid, 0, 0);
+   }
+   else
+   {
+      fprintf(stderr, "error, child1 fork failed\n");
+   }
 }
-
