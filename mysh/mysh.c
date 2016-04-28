720a310562433bd88dd8b250b796fac4920fbe07
Sean Miller <scm84@txstate.edu>
2016-04-28 02:52:56 -0500

    fixed mycd in mysh and updated report

--- mysh/mysh.c
+++ mysh/mysh.c
@@ -58,13 +58,31 @@ int main(int argc, char ** argv) {
 
         //mycd
         else if(!strcmp(args[0], "mycd")) { 
-          if(args[1]) {
-            if(!strcmp(args[1], ".")) chdir(".");
-            else if(!strcmp(args[1], "..")) chdir("..");
-            else {
-              if(chdir(args[1]) != 0) printf("-bash: mycd: %s: No such file or directory\n", args[1]);
-              else chdir(args[1]);
-            }   
+          if(args[1]) 
+		  {
+		  	/*
+            if(!strcmp(args[1], "."))
+			{
+				chdir(".");
+			}
+            else if(!strcmp(args[1], ".."))
+			{
+				chdir("..");
+			}
+            else 
+			{
+			*/
+			    if(chdir(args[1]) != 0)
+				{
+					printf("-bash: mycd: %s: No such file or directory\n", args[1]);
+				}
+				/*
+                else
+				{
+					chdir(args[1]);
+				}
+				*/
+            //}   
           }
           else chdir(getenv("HOME"));
         }
